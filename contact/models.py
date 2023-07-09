ed9671202a887884ac9e2da50feb47c7fac1485d
MatheusSntn <matheussilva2607@hotmail.com.br>
2023-07-08 22:25:26 -0300

    ATUALIZAO

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -9,6 +15,13 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/' ,blank=True)
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+          )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
