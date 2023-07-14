5a97c0b2f4345b52b5b01196d923502a02cc8deb
MatheusSntn <matheussilva2607@hotmail.com.br>
2023-07-13 21:38:11 -0300

    atualizacao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -9,10 +11,30 @@ def index(request):
     
     context = {
         'contacts': contacts,
+        'site_titles': 'Contatos - ' 
     }
 
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+        )
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
